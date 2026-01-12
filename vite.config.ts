0ef8583a4ac3e2df377da2337f89a1687d63d6b7
Praveen Kumar <pra_win98@protonmail.com>
2026-01-12 12:15:57 +0000

    feat: initialize shadcn ui

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import path from 'node:path'
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
@@ -12,4 +13,9 @@ export default defineConfig({
     }),
     tailwindcss()
   ],
+  resolve: {
+	alias: {
+	  "@": path.resolve(__dirname, "./src")
+	}
+  }
 })
